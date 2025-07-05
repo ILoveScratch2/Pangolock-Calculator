1a06565ef700b4505ada03a9c2fced4a188e536c
ILoveScratch <yaowendao2023@qq.com>
2025-07-05 20:27:51 +0800

    initial

---
+++ README.md
@@ -0,0 +1,1 @@
+# Pangolock Calculator
---
+++ android/app/src/main/kotlin/org/dpdns/ilovescratch/pcalc/pangocalc/MainActivity.kt
@@ -0,0 +1,5 @@
+package org.dpdns.ilovescratch.pcalc.pangocalc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
